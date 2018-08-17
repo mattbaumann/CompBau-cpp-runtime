aef75b0b0e8512845e38457f0f00336e5f139916
Matt Baumann <matthias-baumann@outlook.com>
2018-08-17 09:55:58 +0200

    Add linker file and error
    The linker will search and find data types.

---
+++ src/linker/Linker.h
@@ -0,0 +1,11 @@
+#ifndef COMPBAU_CPP_RUNTIME_LINKER_H
+#define COMPBAU_CPP_RUNTIME_LINKER_H
+
+namespace runtime::linker {
+
+    struct Linker {
+
+    };
+}
+
+#endif //COMPBAU_CPP_RUNTIME_LINKER_H
