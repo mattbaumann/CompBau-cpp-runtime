0535993e4fd4c4cafa3db60a5d432f10daf0eb64
Matt Baumann <matthias-baumann@outlook.com>
2018-08-19 16:52:20 +0200

    Add visitor for parse tree
    The visitor interface will allow the linker code to read the parse tree.
    Therefore, it will serve as the API (Front-End) for the parser code.

---
+++ src/parser/parse_tree_visitor.h
@@ -0,0 +1,53 @@
+#ifndef COMPBAU_CPP_RUNTIME_PARSER_VISITOR_H
+#define COMPBAU_CPP_RUNTIME_PARSER_VISITOR_H
+
+#include "parser_model.h"
+
+namespace runtime::parser {
+
+    class parse_tree_visitor {
+
+        virtual void visitModel(parser_model const &model) {
+            for (auto const &i : model.classes) {
+                visitClass(i);
+            }
+        }
+
+        virtual void visitClass(class_type const &classType) {
+            for (auto const &i : classType.field_types) {
+                visitField(i);
+            }
+            for (auto const &i : classType.methods) {
+                visitMethod(i);
+            }
+        }
+
+        virtual void visitField(std::string const &type) {}
+
+        virtual void visitMethod(method const &method) {
+            visitReturn(method.returns);
+            for (auto const &i : method.params) {
+                visitParam(i);
+            }
+            for (auto const &i : method.locals) {
+                visitLocal(i);
+            }
+            for (auto const &i : method.code) {
+                visitInstruction(i);
+            }
+        }
+
+        virtual void visitInstruction(instruction const &instruction) {}
+
+        virtual void visitParam(std::string const &type) {}
+
+        virtual void visitLocal(std::string const &type) {}
+
+        virtual void visitReturn(std::string const &type) {}
+    };
+
+
+}
+
+
+#endif //COMPBAU_CPP_RUNTIME_PARSER_VISITOR_H
