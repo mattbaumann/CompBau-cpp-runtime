aa4e0a93b9a2aa3b21d946d2cac73a2bfbdb52f8
Matt Baumann <matthias-baumann@outlook.com>
2018-09-07 11:20:55 +0200

    Array type
    Corrections of the array descriptor class. It now complies the
    specification. It fully implements the superclass.

--- src/il/array_type.h
+++ src/il/array_type.h
@@ -3,26 +3,33 @@
 
 #include "base_type.h"
 
+#include <string>
+#include <tuple>
+
 namespace runtime::il {
     class array_type : public base_type {
 
-    public:
-        explicit array_type(std::shared_ptr<base_type> decorated_type) : decorated_type{std::move(decorated_type)} {}
+        std::string const length_field_name{"Length"};
 
-        explicit operator std::string() const override {
-            return ((std::string) *decorated_type) + "[]";
+    public:
+        explicit array_type(std::shared_ptr<base_type> decorated_type, std::shared_ptr<base_type> int_type) :
+                decorated_type{std::move(decorated_type)}, int_type{std::move(int_type)} {}
+
+        std::string to_string() const noexcept override {
+            return decorated_type->to_string() + "[]";
         }
 
         std::vector<method> const get_virtual_dispatch() const override {
-            return std::vector<method>();
+            return std::vector<method>{};
         }
 
         std::vector<std::pair<std::string, std::shared_ptr<base_type>>> const get_fields() const override {
-            return std::vector<std::pair<std::string, std::shared_ptr<base_type>>>();
+            return std::vector({std::pair{length_field_name, int_type}});
         }
 
     private:
-        std::shared_ptr<base_type> decorated_type;
+        std::shared_ptr<base_type> const decorated_type;
+        std::shared_ptr<base_type> const int_type;
     };
 }
 #endif //COMPBAU_CPP_RUNTIME_ARRAY_TYPE_H
--- test/il/array_type_test.cpp
+++ test/il/array_type_test.cpp
@@ -4,8 +4,13 @@
 #include "il/build_in_type.h"
 
 TEST_CASE("array_type") {
-    std::shared_ptr<runtime::il::base_type> ptr{new runtime::il::build_in_type{"char"}};
-    runtime::il::array_type test{ptr};
+    std::shared_ptr<runtime::il::base_type> array_elem_type = std::dynamic_pointer_cast<runtime::il::base_type>(
+            std::make_shared<runtime::il::build_in_type>("char")
+    );
+    std::shared_ptr<runtime::il::base_type> array_length_type = std::dynamic_pointer_cast<runtime::il::base_type>(
+            std::make_shared<runtime::il::build_in_type>("int")
+    );
+    runtime::il::array_type test{array_elem_type, array_length_type};
 
     SECTION("it is converted a string representation") {
         std::string const expected{"char[]"};
@@ -17,8 +22,17 @@ TEST_CASE("array_type") {
         REQUIRE(test.get_virtual_dispatch().empty());
     }
 
-    SECTION("fields is empty") {
-        REQUIRE(test.get_fields().empty());
+    SECTION("fields contains length") {
+        int constexpr expectedSize = 1;
+        REQUIRE(expectedSize == test.get_fields().size());
     }
 
+    SECTION("fields length has correct name") {
+        std::string const expectedFieldName{"Length"};
+        REQUIRE(expectedFieldName == test.get_fields().at(0).first);
+    }
+
+    SECTION("fields length has correct type") {
+        REQUIRE(array_length_type == test.get_fields().at(0).second);
+    }
 }
